32e1ab6272dfd6c107c8a89d01fd8932ee832d9a
Pavel Pantus <pantusp@gmail.com>
2016-03-03 19:14:51 -0500

    Update Solution.java
    quick sort using insertion sort with limits

--- Solution.java
+++ Solution.java
@@ -1,33 +1,52 @@
 class Solution {
-    public static void exchange(char[] s, int i1, int i2) {
-      char tmp = s[i1];
-      s[i1] = s[i2];
-      s[i2] = tmp;
+  public static void exchange(char[] s, int i1, int i2) {
+    char tmp = s[i1];
+    s[i1] = s[i2];
+    s[i2] = tmp;
   }
 
   public static char[] quickSortPartition(char[] s) {
-      int i = 1;
-      int j = s.length - 1;
-      char tmp;
+    int i = 1;
+    int j = s.length - 1;
+    char tmp;
 
-      while(i < j) {
-          if (s[i] < s[0]) {
-              i++;
-          }
-
-          if (s[j] > s[0]) {
-              j--;
-          }
-
-          if (s[i] > s[0] && s[j] < s[0]) {
-              exchange(s, i, j);
-          }
+    while(i < j) {
+      if (s[i] < s[0]) {
+        i++;
       }
 
-      exchange(s, 0, i);
-      return s;
+      if (s[j] > s[0]) {
+        j--;
+      }
+
+      if (s[i] > s[0] && s[j] < s[0]) {
+        exchange(s, i, j);
+      }
+    }
+
+    exchange(s, 0, i);
+    return s;
   }
-  
+
+  public static void insertionSortWithLimits(char[] s, int l, int h) {
+    for (int i = l; i < h; i++) {
+      for (int j = i; j > 0; j--) {
+        if (s[j - 1] > s[j]) {
+          char tmp = s[j - 1];
+          s[j - 1] = s[j];
+          s[j] = tmp;
+        }
+      }
+    }
+  }
+
+  public static void quickSort(char[] s) {
+    // TODO: shuffle incoming array;
+    int p = quickSortPartition(s);
+    insertionSortWithLimits(s, 0, p - 1);
+    insertionSortWithLimits(s, p, s.length);
+  }
+
   public static char[] mergeSort(char[] l, char[] r) {
     l = insertionSort(l);
     r = insertionSort(r);
@@ -37,16 +56,16 @@ class Solution {
     char[] aux = new char[l.length + r.length];
 
     for (int k = 0; k < aux.length; k++) {
-        if (li < l.length && (ri == r.length || l[li] <= r[ri])) {
-            aux[k] = l[li++];
-        } else if (ri < r.length && (li == l.length || l[li] > r[ri])) {
-            aux[k] = r[ri++];
-        }
+      if (li < l.length && (ri == r.length || l[li] <= r[ri])) {
+        aux[k] = l[li++];
+      } else if (ri < r.length && (li == l.length || l[li] > r[ri])) {
+        aux[k] = r[ri++];
+      }
     }
 
     return aux;
   }
-    
+
   public static char[] insertionSort(char[] s) {
     for (int i = 0; i < s.length; i++) {
       for (int j = i; j > 0; j--) {
@@ -59,7 +78,7 @@ class Solution {
     }
     return s;
   }
-    
+
   static void reverseInPlace(char[] s) {
     for (int i = 0, j = s.length - 1; i < s.length / 2; i++, j--) {
       char c = s[i];
@@ -79,10 +98,10 @@ class Solution {
         pml = 0;
       }
     }
-    
+
     return -1;
   }
-  
+
   public static void main(String[] args) {
     System.out.println("Index = " + subsBruteForce("abcdef", "ef"));
   }
